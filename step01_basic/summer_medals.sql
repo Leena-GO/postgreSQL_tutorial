54b431ec65964cebbfba5ecc1711f54a185f2077
evan-jung <j2hoon85@gmail.com>
2021-06-09 14:13:20 +0900

    updated

--- step01_basic/summer_medals.sql
+++ step01_basic/summer_medals.sql
@@ -1,18 +1,23 @@
-CREATE TABLE public.summer_medals (
-	year INTEGER,
-	city VARCHAR(42),
-	sport VARCHAR(34),
-	discipline VARCHAR(34),
-	athlete VARCHAR(94),
-	country CHAR(6) NOT NULL,
-	gender VARCHAR(10),
-	event VARCHAR(98),
-	medal VARCHAR(12)
-);
+-- Table: public.summer_medals
+
+-- DROP TABLE public.summer_medals;
+
+CREATE TABLE public.summer_medals
+(
+    year integer,
+    city character varying(42) COLLATE pg_catalog."default",
+    sport character varying(34) COLLATE pg_catalog."default",
+    discipline character varying(34) COLLATE pg_catalog."default",
+    athlete character varying(94) COLLATE pg_catalog."default",
+    country character(6) COLLATE pg_catalog."default",
+    gender character varying(10) COLLATE pg_catalog."default",
+    event character varying(98) COLLATE pg_catalog."default",
+    medal character varying(12) COLLATE pg_catalog."default"
+)
 
 TABLESPACE pg_default;
 
 ALTER TABLE public.summer_medals
     OWNER to postgres;
 
-\COPY summer_medals FROM "step01_basic/data/summer.csv" DELIMITER ',' CSV HEADER;
+\copy summer_medals(year, city, sport, discipline, athlete, country, gender, event, medal) FROM 'step01_basic/data/summer.csv' with DELIMITER ','  CSV HEADER ENCODING 'UTF8';
