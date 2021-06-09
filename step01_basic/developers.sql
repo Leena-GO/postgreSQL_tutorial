de052654d00c1e9e4c845dbdfc6dcd919941c2e6
dschloe <j2hoon85@gmail.com>
2021-06-10 01:04:12 +0900

    new source file updated

--- step01_basic/developers.sql
+++ step01_basic/developers.sql
@@ -11,13 +11,13 @@ ALTER TABLE public.developers
     OWNER to postgres;
 
 INSERT INTO developers VALUES(1, '2019-12-17', '"자바"');
-INSERT INTO developers VALUES(2, '2019-12-17', '''자바''');
-INSERT INTO developers VALUES(3, '2019-12-17', 'Python''s Programming');
+INSERT INTO developers VALUES(2, '2019-12-18', '''자바''');
+INSERT INTO developers VALUES(3, '2019-12-19', 'Python''s Programming');
 
 INSERT INTO developers (book_id, date, name) VALUES
-(4, '2019-12-18', '맛있는 MongoDB'),
-(5, '2019-12-18', 'Python'),
-(6, '2019-12-18', 'R'),
-(7, '2019-12-19', 'Ruby'),
-(8, '2019-12-19', 'Go''s Programming'),
-(9, '2019-12-19', 'Statistics');
+(4, '2020-01-18', '맛있는 MongoDB'),
+(5, '2020-01-19', 'Python'),
+(6, '2020-01-20', 'R'),
+(7, '2021-02-19', 'Ruby'),
+(8, '2021-02-20', 'Go''s Programming'),
+(9, '2021-02-21', 'Statistics');
